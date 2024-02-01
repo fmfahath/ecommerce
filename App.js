894f281aeb24866b8bc8cdf551081d0737d8277d
fmfahath <fmfahath.dev@gmail.com>
2024-02-01 23:17:29 +0530

    dark mode / ligtmode switch added

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View className='flex-1 bg-sky-400'>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className='flex-1 items-center justify-center bg-gray-200 dark:bg-black' >
+      <Switch value={colorScheme === 'dark'} onChange={toggleColorScheme}></Switch>
+      <Text className='dark:text-white'>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
