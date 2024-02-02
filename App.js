d2fb4f7ef87a7665be6a519e2c7bb542704e55aa
fmfahath <fmfahath.dev@gmail.com>
2024-02-02 08:26:07 +0530

    safeArea view npm installed

--- App.js
+++ App.js
@@ -2,19 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Switch, Text, View } from 'react-native';
 import { useColorScheme } from 'nativewind';
 import ProductList from './components/ProductList';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View className='flex-1 items-center justify-center bg-gray-200 dark:bg-black' >
-      <View className='flex-row w-full gap-3 items-center'>
-        <Text className='dark:text-white text-xl font-bold'>New Collection</Text>
-        <Switch value={colorScheme === 'dark'} onChange={toggleColorScheme}></Switch>
+    <SafeAreaView className='flex-1 items-center justify-center bg-gray-200 dark:bg-black'>
+      <View  >
+        <View className='flex-row w-full gap-3 items-center'>
+          <Text className='dark:text-white text-xl font-bold'>New Collection</Text>
+          <Switch value={colorScheme === 'dark'} onChange={toggleColorScheme}></Switch>
+        </View>
+        <ProductList />
+        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
       </View>
-      <ProductList />
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-    </View>
+    </SafeAreaView>
+
   );
 }
 
